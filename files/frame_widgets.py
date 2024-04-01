84cbdeadf1ced67250b2d48b0df6bfdbe5f74770
asif <asahmed8890@gmail.com>
2024-04-01 20:08:36 +0600

    Add files via upload

--- files/frame_widgets.py
+++ files/frame_widgets.py
@@ -3,30 +3,26 @@ import settings
 
 
 class LeftImageButton(ctk.CTkButton):
-    def __init__(self, parent, func,  col, row, image, text='', color='dark-gray'):
+    def __init__(self, parent, func, image, text=''):
         super().__init__(
             master=parent,
             command=func,
-            width=10,
+            width=50,
             height=10,
             text=text,
             image=image,
-            corner_radius=settings.styling['corner_radius'],
             fg_color='transparent',
-            hover_color=settings.COLORS[color]['fg'])
-        self.grid(column=col, row=row, sticky='se', padx=settings.styling['gap'], pady=settings.styling['gap'])
+            hover_color=settings.DARK_GRAY)
 
 
 class RightImageButton(ctk.CTkButton):
-    def __init__(self, parent, func,  col, row, image, text='', color='dark-gray'):
+    def __init__(self, parent, func, image, text=''):
         super().__init__(
             master=parent,
             command=func,
-            width=10,
+            width=50,
             height=10,
             text=text,
             image=image,
-            corner_radius=settings.styling['corner_radius'],
             fg_color='transparent',
-            hover_color=settings.COLORS[color]['fg'])
-        self.grid(column=col, row=row, sticky='se', padx=settings.styling['gap'], pady=settings.styling['gap'])
+            hover_color=settings.DARK_GRAY)
