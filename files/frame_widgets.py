c7e1315c359b24a853d4cb3d49b20f0da2eca487
asif <asahmed8890@gmail.com>
2024-04-04 22:01:16 +0600

    Add files via upload

--- files/frame_widgets.py
+++ files/frame_widgets.py
@@ -38,3 +38,46 @@ class RightImageButton(ctk.CTkButton):
             image=image,
             fg_color='transparent',
             hover_color=settings.DARK_GRAY)
+
+
+class SlidePanel(ctk.CTkFrame):
+    def __init__(self, parent, start_pos, end_pos):
+        super().__init__(master=parent, fg_color='red')
+
+        # General attributes
+        self.start_pos = start_pos
+        self.end_pos = end_pos
+        self.width = abs(start_pos - end_pos)
+
+        # Animation logic
+        self.pos = self.start_pos
+        self.in_start_pos = True
+
+        # Layout
+        self.place(relx=self.start_pos, rely=0.05, relwidth=self.width, relheight=0.9)
+
+    def animate(self):
+        if self.in_start_pos:
+            self.animate_forward()
+        else:
+            self.animate_backwards()
+
+    def animate_forward(self):
+        if self.pos > self.end_pos + 0.008:  # Check if position is greater than the end position + threshold
+            self.pos -= 0.008
+            self.place(relx=self.pos, rely=0.05, relwidth=self.width, relheight=0.9)
+            self.after(10, self.animate_forward)
+        else:
+            self.pos = self.end_pos  # Ensure position reaches end position accurately
+            self.place(relx=self.pos, rely=0.05, relwidth=self.width, relheight=0.9)
+            self.in_start_pos = False
+
+    def animate_backwards(self):
+        if self.pos < self.start_pos - 0.008:  # Check if position is less than the start position - threshold
+            self.pos += 0.008
+            self.place(relx=self.pos, rely=0.05, relwidth=self.width, relheight=0.9)
+            self.after(10, self.animate_backwards)
+        else:
+            self.pos = self.start_pos  # Ensure position reaches start position accurately
+            self.place(relx=self.pos, rely=0.05, relwidth=self.width, relheight=0.9)
+            self.in_start_pos = True
