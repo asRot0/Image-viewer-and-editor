9f2ca38cfb121b9d895428f71959d1c8f1732e4f
asif <asahmed8890@gmail.com>
2024-04-04 13:56:45 +0600

    Add files via upload

--- files/frame_widgets.py
+++ files/frame_widgets.py
@@ -2,6 +2,18 @@ import customtkinter as ctk
 import settings
 
 
+class ImageInfo(ctk.CTkButton):
+    def __init__(self, parent, text, image_info):
+        super().__init__(
+            master=parent,
+            command=image_info,
+            width=10,
+            height=10,
+            text=text,
+            fg_color='transparent',
+            hover_color=None)
+
+
 class LeftImageButton(ctk.CTkButton):
     def __init__(self, parent, func, image, text=''):
         super().__init__(
